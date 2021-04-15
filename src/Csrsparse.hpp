06c67da39dbbd0c50b231d80d86bc3fb8bc2286c
genshen <genshenchu@gmail.com>
2021-04-16 00:44:42 +0800

    feat(compiling): make compiling passed if cmake option `HIP_ENABLE_FLAG` is OFF
    In fact, if `HIP_ENABLE_FLAG` is OFF, `DEVICE_SIDE_VERIFY_FLAG` is also OFF.

--- src/Csrsparse.hpp
+++ src/Csrsparse.hpp
@@ -5,7 +5,14 @@
 #ifndef SPMV_ACC_CSR_SPARSE_H
 #define SPMV_ACC_CSR_SPARSE_H
 
-#include "building_config.h"
 #include "acc/hip/spmv_hip_acc_imp.h"
+#include "building_config.h"
+
+#ifndef ACCELERATE_ENABLED
+// create an empty definition `sparse_spmv`, if HIP is not enabled.
+// just to make compiling passed.
+void sparse_spmv(int htrans, const int halpha, const int hbeta, int hm, int hn, const int *hrowptr,
+                 const int *hcolindex, const double *hvalue, const double *hx, double *hy) {}
+#endif // ACCELERATE_ENABLED
 
 #endif // SPMV_ACC_CSR_SPARSE_H
