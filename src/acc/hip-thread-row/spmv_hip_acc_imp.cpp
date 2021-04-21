6b3798b92a05dfa65ed66650f58743e1ec1881cb
genshen <genshenchu@gmail.com>
2021-04-21 18:58:13 +0800

    style(kernel-thread-row): code format of implementation of thread-row strategy

--- src/acc/hip-thread-row/spmv_hip_acc_imp.cpp
+++ src/acc/hip-thread-row/spmv_hip_acc_imp.cpp
@@ -1,7 +1,7 @@
 //
-// Created by genshen on 2021/4/15.
+// Created by dly on 2021/4/19.
 //
-//spmv_csr_scalar_kernel version 一个线程负责A一行的计算
+// spmv_csr_scalar_kernel version: one thread process one row of matrix A.
 #include <hip/hip_runtime.h>
 #include <hip/hip_runtime_api.h> // hipMalloc, hipMemcpy, etc.
 #include <iostream>
@@ -9,17 +9,15 @@
 #include <stdlib.h> // EXIT_FAILURE
 
 __global__ void device_sparse_spmv_acc(int trans, const int alpha, const int beta, int m, int n, const int *rowptr,
-                                   const int *colindex, const double *value, const double *x, double *y) {
+                                       const int *colindex, const double *value, const double *x, double *y) {
   int thread_id = threadIdx.x + blockDim.x * blockIdx.x;
   for (int i = thread_id; i < m; i += blockDim.x * gridDim.x) {
     double y0 = 0;
-    for (int j = rowptr[i]; j < rowptr[i+1]; j++){
-	y0 += value[j] * x[colindex[j]];
+    for (int j = rowptr[i]; j < rowptr[i + 1]; j++) {
+      y0 += value[j] * x[colindex[j]];
     }
     y[i] = alpha * y0 + beta * y[i];
-    
   }
- 
 }
 
 void sparse_spmv(int htrans, const int halpha, const int hbeta, int hm, int hn, const int *hrowptr,
