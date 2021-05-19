1ddcbccb6ecf85dc40aa1ef4f3544891074e560d
genshen <genshenchu@gmail.com>
2021-05-19 16:21:07 +0800

    feat(demo): update upstream demo code for reading CSR matrix from file

--- src/common_function.hpp
+++ src/common_function.hpp
@@ -1,4 +1,6 @@
 //设置计算类型为双精度
+#include <iomanip>
+#include <vector>
 
 using namespace std;
 typedef double dtype;
@@ -20,7 +22,6 @@ int *rowptr = NULL;              // csr行标识
 int a = 0;                       //稀疏矩阵非零值个数
 int n = 0;                       //矩阵维度
 int m = 0;                       //矩阵维度
-float s = 0.5;                   //稠密度
 int A_nnz = 0;                   //同a
 int A_num_cols = 0;              //同m
 int A_num_rows = 0;              //同n
@@ -29,38 +30,52 @@ double *dA_values, *dX, *dY;
 double alpha = 1; //稀疏矩阵的标量系数
 double beta = 1;  //标量系数
 
-int rand_integer(int min, int max) { return int((rand() % (max - min + 1)) + min); }
+//新demo程序从外部读取csr数据，使用vector装载程序
+vector<double> csr_data;
+vector<int> csr_indices;
+vector<int> csr_indptr;
+vector<double> dense_vector;
 
-double rand_double(double min, double max) {
-  double temp = min + (max - min) * double(rand() % 100) / double((101));
-  // cout<<temp<<endl;
-  return temp;
+template <typename T> void init_csr_dense_vector(char *buf, vector<T> &vec) {
+  char *p;
+  p = strtok(buf, " ");
+  while (p != NULL) {
+    // printf("%s\n", p);
+    bool isInt = std::is_same<T, int>::value;
+    if (isInt == true)
+      vec.push_back(atoi(p));
+    else
+      vec.push_back(atof(p));
+    p = strtok(NULL, " ");
+  }
 }
 
-int matrix_to_csr(int m, int n, dtype **M, dtype *&value, int *&rowptr, int *&colindex) {
-  int i, j;
-  int a = 0;
-  for (i = 0; i < m; i++)
-    for (j = 0; j < n; j++)
-      if (M[i][j] != 0)
-        a++;
-  value = new dtype[a];
-  colindex = new int[a];
-  rowptr = new int[n + 1];
-  int k = 0;
-  int l = 0;
-  for (i = 0; i < m; i++)
-    for (j = 0; j < n; j++) {
-      if (j == 0)
-        rowptr[l++] = k;
-      if (M[i][j] != 0) {
-        value[k] = M[i][j];
-        colindex[k] = j;
-        k++;
-      }
-    }
-  rowptr[l] = a;
-  return a;
+template <typename T> void print_vector(vector<T> &vecTest) {
+  for (auto it : vecTest) {
+    cout << setprecision(10) << it << " ";
+  }
+  cout << endl << endl;
+}
+
+void read_file(char *path) {
+
+  FILE *in = fopen(path, "r");
+  char buf[1024 * 1024 * 100]; // 100mbuff
+  int line_num = 0;
+  while (fgets(buf, sizeof(buf), in) != NULL) {
+    // printf("%s\n\n", buf);
+
+    if (line_num == 1)
+      init_csr_dense_vector<double>(buf, csr_data);
+    if (line_num == 2)
+      init_csr_dense_vector<int>(buf, csr_indices);
+    if (line_num == 3)
+      init_csr_dense_vector<int>(buf, csr_indptr);
+    if (line_num == 4)
+      init_csr_dense_vector<double>(buf, dense_vector);
+    line_num++;
+  }
+  fclose(in);
 }
 
 void spmv(int alpha, int beta, dtype *value, int *rowptr, int *colindex, int m, int n, int a, dtype *x, dtype *y) {
@@ -75,29 +90,10 @@ void spmv(int alpha, int beta, dtype *value, int *rowptr, int *colindex, int m, 
   return;
 }
 
-void generate_sparse_matrix(dtype **&ptr, int m, int n, double s) {
-  ptr = new dtype *[m];
-  for (int i = 0; i < m; i++)
-    ptr[i] = new dtype[n];
-  for (int i = 0; i < m; i++)
-    for (int j = 0; j < n; j++) {
-      double x = rand_double(-1, 1);
-      if (x > (-1 + 2 * s))
-        ptr[i][j] = 0;
-      else
-        ptr[i][j] = x;
-    }
-  return;
-}
-
-void print_matrix(dtype **ptr, int m, int n) {
-  for (int i = 0; i < m; i++)
-    for (int j = 0; j < n; j++) {
-      cout << ptr[i][j] << ",";
-      if (j == n - 1)
-        cout << endl;
-    }
-  return;
+double rand_double(double min, double max) {
+  double temp = min + (max - min) * double(rand() % 100) / double((101));
+  // cout<<temp<<endl;
+  return temp;
 }
 
 void generate_vector(int n, dtype *&x) {
@@ -138,17 +134,25 @@ void print_vector(int n, int *x) {
   }
 
 void create_host_data() {
-  generate_sparse_matrix(mat, m, n, s);
-  generate_vector(n, hX);
+  generate_vector(n, hX); //外部读取数据覆盖hX
   generate_vector(m, temphY);
-  generate_vector(m, hY);
+  generate_vector(m, hY); //实际数据依然来自随机生成
   generate_vector(m, hhY);
-  a = matrix_to_csr(m, n, mat, value, rowptr, colindex); //非零元素的个数
+
   //统一主机端和设备端的向量值
   memcpy(hY, temphY, m * sizeof(double));
   memcpy(hhY, temphY, m * sizeof(double));
+
+  //重新初始化，将读到的数据拷贝到之前定义的数组减少改动量
+  value = new dtype[csr_data.size()];
+  colindex = new int[csr_indices.size()];
+  rowptr = new int[csr_indptr.size()];
+  a = csr_data.size();
+  memcpy(hX, &dense_vector[0], n * sizeof(double));
+  memcpy(colindex, &csr_indices[0], csr_indices.size() * sizeof(int));
+  memcpy(rowptr, &csr_indptr[0], csr_indptr.size() * sizeof(int));
+  memcpy(value, &csr_data[0], csr_data.size() * sizeof(double));
   // debug
-  //  print_matrix(mat,m,n);
   //  print_vector(n,hX);
 }
 
@@ -179,15 +183,17 @@ void create_deivce_data() {
 }
 
 void verify(double *dy, double *hy, int n) {
+  int total_validation = 0;
   for (int i = 0; i < n; i++) {
-    if (fabs(dy[i] - hy[i]) >= 1e-10) {
-      cout << "i:" << i << " dy[i]:" << dy[i] << " hy[i]:" << hy[i] << endl;
+    if (fabs(dy[i] - hy[i]) / fabs(hy[i]) >= 1e-7) {
+      cout << fabs(dy[i] - hy[i]) << " i:" << i << " dy[i]:" << dy[i] << " hy[i]:" << hy[i] << endl;
       cout << "Failed verification,please check your code\n" << endl;
       return;
     }
-    cout << "i:" << i << " dy[i]:" << dy[i] << " hy[i]:" << hy[i] << endl;
+    total_validation = i;
+    //  cout<<"i:"<<i<<" dy[i]:"<<dy[i]<<" hy[i]:"<<hy[i]<<endl;
   }
-  cout << "Congratulation,pass validation!\n" << endl;
+  cout << "Congratulation,pass " << total_validation + 1 << " validation!\n" << endl;
 }
 
 enum sparse_operation operation = operation_none;
@@ -207,6 +213,7 @@ void rocsparse() {
   ROCSPARSE_CHECK(rocsparse_create_dnvec_descr(&vecX, A_num_cols, dX, rocsparse_datatype_f64_r));
   ROCSPARSE_CHECK(rocsparse_create_dnvec_descr(&vecY, A_num_rows, dY, rocsparse_datatype_f64_r));
   // allocate an external buffer if needed
+
   ROCSPARSE_CHECK(rocsparse_spmv(handle, rocsparse_operation_none, &alpha, matA, vecX, &beta, vecY,
                                  rocsparse_datatype_f64_r, rocsparse_spmv_alg_default, &bufferSize, nullptr));
   HIP_CHECK(hipMalloc(&dBuffer, bufferSize));
