2fc10128f09486780a9dc64a8fc267725099510c
genshen <genshenchu@gmail.com>
2021-05-19 17:33:44 +0800

    feat(examples): update sbatch script to run all test cases

--- examples/batch.sh
+++ examples/batch.sh
@@ -18,4 +18,33 @@ module load compiler/rocm/3.9.1
 
 #ldd ${BIN_PATH}
 ulimit -c unlimited
+
+echo "23560 * 23560"
 ${BIN_PATH} ./data/af23560.csr
+
+echo "13436 * 13436"
+${BIN_PATH} ./data/bayer10.csr
+
+echo "11948 * 11948"
+${BIN_PATH} ./data/bcsstk18.csr
+
+echo "9540 * 9540"
+${BIN_PATH} ./data/coater2.csr
+
+echo "8192 * 8192"
+${BIN_PATH} ./data/dw4096.csr
+
+echo "14734 * 14734"
+${BIN_PATH} ./data/epb1.csr
+
+echo "6001 * 6001"
+${BIN_PATH} ./data/exdata_1.csr
+
+echo "9506 * 9506"
+${BIN_PATH} ./data/nemeth03.csr
+
+echo "15575 * 15575"
+${BIN_PATH} ./data/poli_large.csr
+
+echo "7602 * 7602"
+${BIN_PATH} ./data/rajat03.csr
