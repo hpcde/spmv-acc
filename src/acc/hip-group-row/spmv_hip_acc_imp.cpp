8d9a2a81a66f5b862b03f3036636477ae29d2cc4
genshen <genshenchu@gmail.com>
2021-06-01 20:20:30 +0800

    refactor(kernel-grpup-row): variables renaming of group-row and light imp to satisfy the code style

--- src/acc/hip-group-row/spmv_hip_acc_imp.cpp
+++ src/acc/hip-group-row/spmv_hip_acc_imp.cpp
@@ -18,35 +18,32 @@
  *
  * @tparam GROUP_SIZE threads in on group
  * @tparam WF_SIZE threads in one wavefront
- * @param size rows in matrix A
+ * @param m rows in matrix A
  * @param alpha alpha value
  * @param beta beta value
- * @param ia row offset array of csr matrix A
- * @param ja col index of csr matrix A
- * @param va matrix A in csr format
+ * @param row_offset row offset array of csr matrix A
+ * @param csr_col_ind col index of csr matrix A
+ * @param csr_val matrix A in csr format
  * @param x vector x
  * @param y vector y
  * @return
  */
 template <int GROUP_SIZE, int WF_SIZE>
-__global__ void spmv_group_row_kernel(int trans, const int alpha, const int beta, const int *ia, const int *ja,
-                                      const double *va, const double *x, double *y, int size) {
-
-  int globalThreadId = threadIdx.x + blockDim.x * blockIdx.x;
-  int groupThreadId = globalThreadId % GROUP_SIZE;           // local thread id in current group
-  const int group_id = globalThreadId / GROUP_SIZE;          // global group id
+__global__ void spmv_group_row_kernel(int m, const int alpha, const int beta, const int *row_offset,
+                                      const int *csr_col_ind, const double *csr_val, const double *x, double *y) {
+  const int global_thread_id = threadIdx.x + blockDim.x * blockIdx.x;
+  const int group_thread_id = global_thread_id % GROUP_SIZE; // local thread id in current group
+  const int group_id = global_thread_id / GROUP_SIZE;        // global group id
   const int group_num = gridDim.x * blockDim.x / GROUP_SIZE; // total groups on device
-  int rowStart, rowEnd;
-  double sum;
 
   int row = group_id;
-  for (row = group_id; row < size; row += group_num) {
-    rowStart = ia[row];
-    rowEnd = ia[row + 1];
-    sum = 0;
+  for (row = group_id; row < m; row += group_num) {
+    const int row_start = row_offset[row];
+    const int row_end = row_offset[row + 1];
+    double sum = 0;
 
-    for (int i = rowStart + groupThreadId; i < rowEnd; i += GROUP_SIZE) {
-      sum += va[i] * x[ja[i]];
+    for (int i = row_start + group_thread_id; i < row_end; i += GROUP_SIZE) {
+      sum += csr_val[i] * x[csr_col_ind[i]];
     }
 
     // reduce inside a group
@@ -54,29 +51,28 @@ __global__ void spmv_group_row_kernel(int trans, const int alpha, const int beta
       sum += __shfl_down(sum, i, GROUP_SIZE);
     }
 
-    if (groupThreadId == 0) {
+    if (group_thread_id == 0) {
       y[row] = alpha * sum + beta * y[row];
     }
   }
 }
 
 #define GROUP_KERNEL_WRAPPER(N)                                                                                        \
-  (spmv_group_row_kernel<N, 64>)<<<256, 256>>>(trans, alpha, beta, rowptr, colindex, value, x, y, m)
+  (spmv_group_row_kernel<N, 64>)<<<256, 256>>>(m, alpha, beta, rowptr, colindex, value, x, y)
 
 void sparse_spmv(int trans, const int alpha, const int beta, int m, int n, const int *rowptr, const int *colindex,
                  const double *value, const double *x, double *y) {
+  const int avg_eles_per_row = rowptr[m] / m;
 
-  const int mean_eles_per_row = rowptr[m] / m;
-
-  if (mean_eles_per_row <= 4) {
+  if (avg_eles_per_row <= 4) {
     GROUP_KERNEL_WRAPPER(2);
-  } else if (mean_eles_per_row <= 8) {
+  } else if (avg_eles_per_row <= 8) {
     GROUP_KERNEL_WRAPPER(4);
-  } else if (mean_eles_per_row <= 16) {
+  } else if (avg_eles_per_row <= 16) {
     GROUP_KERNEL_WRAPPER(8);
-  } else if (mean_eles_per_row <= 32) {
+  } else if (avg_eles_per_row <= 32) {
     GROUP_KERNEL_WRAPPER(16);
-  } else if (mean_eles_per_row <= 64) {
+  } else if (avg_eles_per_row <= 64) {
     GROUP_KERNEL_WRAPPER(32);
   } else {
     GROUP_KERNEL_WRAPPER(64);
--- src/acc/hip-light/spmv_hip_acc_imp.cpp
+++ src/acc/hip-light/spmv_hip_acc_imp.cpp
@@ -20,29 +20,27 @@
  *
  * @tparam THREADS_PER_VECTOR threads in on vector
  * @tparam WF_SIZE threads in one wavefront
- * @param size rows in matrix A
+ * @param m rows in matrix A
  * @param alpha alpha value
  * @param beta beta value
  * @param hip_row_counter shared GRM data
- * @param ia row offset array of csr matrix A
- * @param ja col index of csr matrix A
- * @param va matrix A in csr format
+ * @param row_offset row offset array of csr matrix A
+ * @param csr_col_ind col index of csr matrix A
+ * @param csr_val matrix A in csr format
  * @param x vector x
  * @param y vector y
  * @return
  */
 template <int THREADS_PER_VECTOR, int WF_SIZE>
-__global__ void spmv_light_kernel(int trans, const int alpha, const int beta, int *hip_row_counter, const int *ia,
-                                  const int *ja, const double *va, const double *x, double *y, int size) {
-
-  int i;
-  double sum;
-  int row;
-  int row_start, row_end;
+__global__ void spmv_light_kernel(const int m, const int alpha, const int beta, int *hip_row_counter,
+                                  const int *row_offset, const int *csr_col_ind, const double *csr_val,
+                                  const double *x, double *y) {
   const int land_id = threadIdx.x % THREADS_PER_VECTOR;  // land_id in current vector
   const int wf_land_id = threadIdx.x & (WF_SIZE - 1);    // land_id in current wavefront
   const int wf_vec_id = wf_land_id / THREADS_PER_VECTOR; // vector id in current wavefront
 
+  int i;
+  int row;
   // Each time, in each wavefront, it will consume {vectors in one wavefront} rows.
   if (wf_land_id == 0) {
     // atomicAdd is only for the first thread in wavefront
@@ -52,12 +50,12 @@ __global__ void spmv_light_kernel(int trans, const int alpha, const int beta, in
   __syncthreads();
 
   while (row < size) {
-    row_start = ia[row];
-    row_end = ia[row + 1];
-    sum = 0;
+    const int row_start = row_offset[row];
+    const int row_end = row_offset[row + 1];
+    double sum = 0;
 
     for (i = row_start + land_id; i < row_end; i += THREADS_PER_VECTOR) {
-      sum += va[i] * x[ja[i]];
+      sum += csr_col_ind[i] * x[csr_col_ind[i]];
     }
 
     // reduction
@@ -79,28 +77,27 @@ __global__ void spmv_light_kernel(int trans, const int alpha, const int beta, in
 }
 
 #define LIGHT_KERNEL_CALLER(N)                                                                                         \
-  ((spmv_light_kernel<N, 64>) <<<256, 256>>>                                                                           \
-   (trans, alpha, beta, hip_row_counter, rowptr, colindex, value, x, y, m))
+  ((spmv_light_kernel<N, 64>) <<<256, 256>>> (m, alpha, beta, hip_row_counter, rowptr, colindex, value, x, y))
 
-void sparse_spmv(int trans, const int alpha, const int beta, int m, int n, const int *rowptr, const int *colindex,
+void sparse_spmv(int m, const int alpha, const int beta, int m, int n, const int *rowptr, const int *colindex,
                  const double *value, const double *x, double *y) {
 
   int *hip_row_counter;
   hipMalloc((void **)&hip_row_counter, sizeof(int));
-  const int mean_eles_per_row = rowptr[m] / m;
+  const int avg_eles_per_row = rowptr[m] / m;
   hipMemset(hip_row_counter, 0, sizeof(int));
 
-  if (mean_eles_per_row <= 2) {
+  if (avg_eles_per_row <= 2) {
     LIGHT_KERNEL_CALLER(1);
-  } else if (mean_eles_per_row <= 4) {
+  } else if (avg_eles_per_row <= 4) {
     LIGHT_KERNEL_CALLER(2);
-  } else if (mean_eles_per_row <= 8) {
+  } else if (avg_eles_per_row <= 8) {
     LIGHT_KERNEL_CALLER(4);
-  } else if (mean_eles_per_row <= 16) {
+  } else if (avg_eles_per_row <= 16) {
     LIGHT_KERNEL_CALLER(8);
-  } else if (mean_eles_per_row <= 32) {
+  } else if (avg_eles_per_row <= 32) {
     LIGHT_KERNEL_CALLER(16);
-  } else if (mean_eles_per_row <= 64) {
+  } else if (avg_eles_per_row <= 64) {
     LIGHT_KERNEL_CALLER(32);
   } else {
     LIGHT_KERNEL_CALLER(64);
