6378eed40c3c90d406934fac5eee95b41718828e
genshen <genshenchu@gmail.com>
2021-06-29 23:43:03 +0800

    feat(tools): better cli and sub-command "dist" to show nnz distribution

--- tools/main.cpp
+++ tools/main.cpp
@@ -1,8 +1,9 @@
 #include <iomanip>
 #include <iostream>
-#include <stdio.h>  // printf
-#include <stdlib.h> // EXIT_FAILURE
-#include <string.h>
+#include <map>
+#include <cstdio>  // printf
+#include <cstdlib> // EXIT_FAILURE
+#include <cstring>
 #include <type_traits>
 #include <vector>
 
@@ -21,8 +22,11 @@ template <typename T> void init_csr_dense_vector(char *buf, std::vector<T> &vec)
   }
 }
 
-void read_file(char *path) {
+bool read_file(char *path) {
   FILE *in = fopen(path, "r");
+  if (in == nullptr) {
+    return false;
+  }
   constexpr long buffer_size = 1024L * 1024L * 1024L * 5L;
   char *buf = new char[buffer_size]; // 5g buff
   int line_num = 0;
@@ -40,27 +44,82 @@ void read_file(char *path) {
   }
   delete[] buf;
   fclose(in);
+  return true;
 }
 
+enum SubCmd { nnz = 0, dist = 1 };
+
 int main(int argc, char **argv) {
+  const std::string usage = R"""(Usage:
+  csv-reader nnz [file-path]
+  csv-reader dist [file-path]
+  csv-reader --help
+)""";
   if (argc < 2) {
-    printf("Usage: %s [file-path]\n", argv[0]);
+    std::cout << usage;
     return 0;
   }
-  char path[1024];
-  strcpy(path, argv[1]);
-  read_file(path);
+  const std::string sub_cmd_str = std::string(argv[1]);
+  if (sub_cmd_str == "--help") {
+    std::cout << usage;
+    return 0;
+  }
+  SubCmd sub_cmd;
+  if (sub_cmd_str == "nnz") {
+    sub_cmd = nnz;
+  } else if (sub_cmd_str == "dist") {
+    sub_cmd = dist;
+  } else {
+    std::cerr << "unknown sub-command " << sub_cmd_str << std::endl;
+    std::cout << usage;
+    return 1;
+  }
+  if (argc < 3) {
+    std::cerr << "missing file path." << std::endl;
+    return 1;
+  }
 
-  long i = 0;
-  int last_row = 0;
-  int last_diff = 0;
-  for (int row_offset : csr_indptr) {
-    if (i != 0) {
-      std::cout << row_offset - last_row << std::endl;
+  char path[1024];
+  strcpy(path, argv[2]);
+  if (!read_file(path)) {
+    std::cerr << "reading file error." << std::endl;
+    return 1;
+  }
+
+  int m = csr_indptr.size() - 1;
+  int n = dense_vector.size();
+
+  if (sub_cmd == nnz) {
+    bool is_first = true;
+    int last_row = 0;
+    for (int row_offset : csr_indptr) {
+      if (!is_first) {
+        std::cout << row_offset - last_row << std::endl;
+      }
+      last_row = row_offset;
+      is_first = false;
     }
-    last_diff = row_offset - last_row;
-    last_row = row_offset;
-    i++;
+  } else {
+    bool is_first = true;
+    int last_row = 0;
+    std::map<int, int> K;
+    for (int row_offset : csr_indptr) {
+      if (!is_first) {
+        const int diff = row_offset - last_row;
+        //      std::cout << row_offset - last_row << std::endl;
+        if (K.find(diff) != K.end()) {
+          K[diff] += 1;
+        } else {
+          K[diff] = 1;
+        }
+      }
+      last_row = row_offset;
+      is_first = false;
+    }
+
+    for (const auto &[key, value] : K) {
+      std::cout << key << " = " << value << "\n";
+    }
   }
   return 0;
 }
