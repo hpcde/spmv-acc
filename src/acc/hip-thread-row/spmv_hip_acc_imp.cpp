19c482d7a8699b555c9fb49e0a0c7a5b4c54dfc6
genshen <genshenchu@gmail.com>
2021-07-12 20:47:26 +0800

    refactor(kernel-thread-row): rename kernel func from device_sparse_spmv_acc to native_thread_row

--- src/acc/hip-thread-row/spmv_hip_acc_imp.cpp
+++ src/acc/hip-thread-row/spmv_hip_acc_imp.cpp
@@ -2,15 +2,16 @@
 // Created by dly on 2021/4/19.
 //
 // spmv_csr_scalar_kernel version: one thread process one row of matrix A.
-#include <hip/hip_runtime.h>
-#include <hip/hip_runtime_api.h> // hipMalloc, hipMemcpy, etc.
+
 #include <iostream>
 #include <stdio.h>  // printf
 #include <stdlib.h> // EXIT_FAILURE
 
-__global__ void device_sparse_spmv_acc(int trans, const double alpha, const double beta, int m, int n,
-                                       const int *rowptr, const int *colindex, const double *value, const double *x,
-                                       double *y) {
+#include <hip/hip_runtime.h>
+#include <hip/hip_runtime_api.h> // hipMalloc, hipMemcpy, etc.
+
+__global__ void native_thread_row(int trans, const double alpha, const double beta, int m, int n, const int *rowptr,
+                                  const int *colindex, const double *value, const double *x, double *y) {
   int thread_id = threadIdx.x + blockDim.x * blockIdx.x;
   const int next_row_step = blockDim.x * gridDim.x;
   double y0 = 0.0;
@@ -25,5 +26,5 @@ __global__ void device_sparse_spmv_acc(int trans, const double alpha, const doub
 
 void sparse_spmv(int htrans, const int halpha, const int hbeta, int hm, int hn, const int *hrowptr,
                  const int *hcolindex, const double *hvalue, const double *hx, double *hy) {
-  device_sparse_spmv_acc<<<1, 1024>>>(htrans, halpha, hbeta, hm, hn, hrowptr, hcolindex, hvalue, hx, hy);
+  native_thread_row<<<1, 1024>>>(htrans, halpha, hbeta, hm, hn, hrowptr, hcolindex, hvalue, hx, hy);
 }
