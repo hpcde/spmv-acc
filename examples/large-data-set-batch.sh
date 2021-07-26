ef77bc774d881f74afc72ce1104afdaa81197580
genshen <genshenchu@gmail.com>
2021-07-26 20:40:48 +0800

    chore(examples): reorder the case in alphabetical order and correct cases information

--- examples/large-data-set-batch.sh
+++ examples/large-data-set-batch.sh
@@ -21,32 +21,32 @@ echo $SLURM_JOB_NODELIST
 ulimit -c unlimited
 
 # rows, cols, non-zeros, percent of non-zeros, average non-zeros per row
-echo "boneS10: (914,898	914,898	40,878,708	0.00488%	44.68116)"
+echo "boneS10: (914,898/914,898 28,191,660 0.00337% 30.81)"
 ${BIN_PATH} ./large-data-set/boneS10.csr
 
-echo "Cube_Coup_dt6: (2,911,419	2,911,419	127,729,899	0.00151%	43.87204)"
-${BIN_PATH} ./large-data-set/Cube_Coup_dt6.csr
-
-echo "Ga41As41H72: (2,164,760	2,164,760	124,406,070	0.00265%	57.46876)"
-${BIN_PATH} ./large-data-set/Ga41As41H72.csr
-
-echo "largebasis: (1,102,824	1,102,824	89,306,020	0.00734%	80.97939)"
-${BIN_PATH} ./large-data-set/largebasis.csr
-
-echo "TSOPF_RS_b2383: (268,096	268,096	18,488,476	0.02572%	68.96215)"
-${BIN_PATH} ./large-data-set/TSOPF_RS_b2383.csr
-
-echo "Bump_2911: (8,217,820	7,591,564	40,451,632	0.00006%	5.32850)"
+echo "Bump_2911: (2,911,419/2,911,419 65,320,659 0.00077% 22.44)"
 ${BIN_PATH} ./large-data-set/Bump_2911.csr
 
-echo "dielFilterV3real: (440,020	440,020	5,240,084	0.00271%	11.90874)"
+echo "Cube_Coup_dt6: (2,164,760/2,164,760 64,685,452 0.00138% 29.88)"
+${BIN_PATH} ./large-data-set/Cube_Coup_dt6.csr
+
+echo "dielFilterV3real: (1,102,824/1,102,824 45,204,422 0.00372% 40.99)"
 ${BIN_PATH} ./large-data-set/dielFilterV3real.csr
 
-echo "Hardesty3: (381,689	381,689	37,464,962	0.02572%	98.15573)"
+echo "Ga41As41H72: (268,096/268,096 9,378,286 0.01305% 34.98)"
+${BIN_PATH} ./large-data-set/Ga41As41H72.csr
+
+echo "Hardesty3: (8,217,820/7,591,564 40,451,632 0.00006% 4.92)"
 ${BIN_PATH} ./large-data-set/Hardesty3.csr
 
-echo "RM07R: (38,120	38,120	16,171,169	1.11285%	424.21744)"
+echo "largebasis: (440,020/440,020 5,560,100 0.00287% 12.64)"
+${BIN_PATH} ./large-data-set/largebasis.csr
+
+echo "RM07R: (381,689/381,689 37,464,962 0.02572% 98.16)"
 ${BIN_PATH} ./large-data-set/RM07R.csr
 
-echo "vas_stokes_2M: (2,146,677	2,146,677	65,129,037	0.00141%	30.33947)"
+echo "TSOPF_RS_b2383: (38,120/38,120 16,171,169 1.11285% 424.22)"
+${BIN_PATH} ./large-data-set/TSOPF_RS_b2383.csr
+
+echo "vas_stokes_2M: (2,146,677/2,146,677 65,129,037 0.00141% 30.34)"
 ${BIN_PATH} ./large-data-set/vas_stokes_2M.csr
