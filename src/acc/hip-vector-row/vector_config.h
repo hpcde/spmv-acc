cca00741925bb940e16b3d4ad559c9ad12de1c63
genshen <genshenchu@gmail.com>
2021-08-07 23:09:02 +0800

    perf(kernel-vector-row): add vector y memory coalescing support for adaptive vector-row strategy
    note: this feature is disabled by default. It can be enabled via macro
    `ADAPTIVE_Y_MEMORY_COALESCING`

--- src/acc/hip-vector-row/vector_config.h
+++ src/acc/hip-vector-row/vector_config.h
@@ -11,4 +11,7 @@
 
 #define MEMORY_ACCESS_Y_COALESCING
 
+// memory coalescing for adaptive vector row (with data block dividing), default is disabled.
+//#define ADAPTIVE_Y_MEMORY_COALESCING
+
 #endif // SPMV_ACC_VEOTR_ROW_CONFIG_H
