83d4dbdc75dd2bc6183abe308435b372087f61b5
genshen <genshenchu@gmail.com>
2021-08-10 14:02:01 +0800

    feat(kernel-thread-row): apply vector x reampping when macro `OPT_THREAD_ROW_REMAP_VEC_X` is defined

--- src/acc/hip-thread-row/thread_row.cpp
+++ src/acc/hip-thread-row/thread_row.cpp
@@ -3,14 +3,23 @@
 //
 
 #include "thread_row.h"
+#include "thread_row_config.h"
 
 void thread_row_sparse_spmv(int trans, const int alpha, const int beta, int m, int n, const int *d_row_ptr,
                             const int *d_csr_col_index, const double *d_csr_value, const double *d_x, double *d_y) {
   const int avg_nnz_per_row = d_row_ptr[m] / m;
   if (avg_nnz_per_row <= 4) {
     constexpr int MAX_ROW_NNZ = 5; // 5 is up bound.
-    (kernel_thread_row<1, MAX_ROW_NNZ, 64, 256, int, double>)<<<53 * 60, 256>>>(alpha, beta, m, d_row_ptr,
-                                                                                d_csr_col_index, d_csr_value, d_x, d_y);
+#ifndef OPT_THREAD_ROW_REMAP_VEC_X
+    constexpr int BLOCKS = 53 * 60;
+    (kernel_thread_row<1, MAX_ROW_NNZ, 64, 256, int, double>)<<<BLOCKS, 256>>>(alpha, beta, m, d_row_ptr,
+                                                                               d_csr_col_index, d_csr_value, d_x, d_y);
+#endif
+#ifdef OPT_THREAD_ROW_REMAP_VEC_X
+    constexpr int BLOCKS = 53 * 60;
+    (kernel_thread_row_v2<1, MAX_ROW_NNZ, 64, 256, int, double>)<<<BLOCKS, 256>>>(
+        alpha, beta, m, d_row_ptr, d_csr_col_index, d_csr_value, d_x, d_y);
+#endif
   } else {
     native_thread_row<<<1, 1024>>>(trans, alpha, beta, m, n, d_row_ptr, d_csr_col_index, d_csr_value, d_x, d_y);
   }
--- src/acc/hip-thread-row/thread_row_config.h
+++ src/acc/hip-thread-row/thread_row_config.h
@@ -7,4 +7,11 @@
 
 #define THREAD_ROW_GLOBAL_LOAD_X2 // if defined, we load 2 double or 2 int in each loop.
 
+// remapping memory accessing mode of vector x.
+// In most cases, the matrix is a diagonal matrix.
+// Thus, it is better to access vector x with column first mode.
+// If the macro below is enabled, the memory accessing mode of vector x is column-first,
+// otherwise, the mode is row-first.
+#define OPT_THREAD_ROW_REMAP_VEC_X
+
 #endif // SPMV_ACC_THREAD_ROW_CONFIG_H
