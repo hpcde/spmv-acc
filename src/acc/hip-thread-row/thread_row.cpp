e3bc507c9ff216545fa6c13ea13f8c744154b57d
genshen <genshenchu@gmail.com>
2021-08-10 15:51:32 +0800

    perf(kernel-thread-row): tune grid dim for native thread-row and optimization "vec x remapping"

--- src/acc/hip-thread-row/thread_row.cpp
+++ src/acc/hip-thread-row/thread_row.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "thread_row.h"
+#include "building_config.h"
 #include "thread_row_config.h"
 
 void thread_row_sparse_spmv(int trans, const int alpha, const int beta, int m, int n, const int *d_row_ptr,
@@ -11,17 +12,17 @@ void thread_row_sparse_spmv(int trans, const int alpha, const int beta, int m, i
   if (avg_nnz_per_row <= 4) {
     constexpr int MAX_ROW_NNZ = 5; // 5 is up bound.
 #ifndef OPT_THREAD_ROW_REMAP_VEC_X
-    constexpr int BLOCKS = 53 * 60;
+    constexpr int BLOCKS = 53 * AVAILABLE_CU;
     (kernel_thread_row<1, MAX_ROW_NNZ, 64, 256, int, double>)<<<BLOCKS, 256>>>(alpha, beta, m, d_row_ptr,
                                                                                d_csr_col_index, d_csr_value, d_x, d_y);
 #endif
 #ifdef OPT_THREAD_ROW_REMAP_VEC_X
-    constexpr int BLOCKS = 53 * 60;
+    constexpr int BLOCKS = 112 * AVAILABLE_CU;
     (kernel_thread_row_v2<1, MAX_ROW_NNZ, 64, 256, int, double>)<<<BLOCKS, 256>>>(
         alpha, beta, m, d_row_ptr, d_csr_col_index, d_csr_value, d_x, d_y);
 #endif
   } else {
-    native_thread_row<<<1, 1024>>>(trans, alpha, beta, m, n, d_row_ptr, d_csr_col_index, d_csr_value, d_x, d_y);
+    native_thread_row<<<128, 1024>>>(trans, alpha, beta, m, n, d_row_ptr, d_csr_col_index, d_csr_value, d_x, d_y);
   }
   return;
 }
