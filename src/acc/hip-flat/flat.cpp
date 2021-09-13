064fc2de973eda3200e298eb5447ee6edf5b79cd
genshen <genshenchu@gmail.com>
2021-09-13 19:57:04 +0800

    fix(kernel-flat): correct the wrong length of array `break_points`
    Note: this bug dose not effect performance or correctness.

--- src/acc/hip-flat/flat.cpp
+++ src/acc/hip-flat/flat.cpp
@@ -9,15 +9,15 @@ void flat_sparse_spmv(int trans, const int alpha, const int beta, int m, int n, 
   constexpr int blocks = 512;
   constexpr int threads_per_block = 512;
   int *break_points;
-  // the nnz is rowptr[m], in one round, it can process about `blocks * threads_per_block` nnz.
+  // the nnz is rowptr[m], in one round, it can process about `blocks * R * threads_per_block` nnz.
+  constexpr int R = 2;
   const int total_rounds =
-      rowptr[m] / (threads_per_block * blocks) + (rowptr[m] % (threads_per_block * blocks) == 0 ? 0 : 1);
+      rowptr[m] / (R * threads_per_block * blocks) + (rowptr[m] % (R * threads_per_block * blocks) == 0 ? 0 : 1);
   // each round and each block both have a break point.
   const int break_points_len = total_rounds * blocks + 1;
   hipMalloc((void **)&break_points, break_points_len * sizeof(int));
   hipMemset(break_points, 0, break_points_len * sizeof(int));
 
-  constexpr int R = 2;
   (pre_calc_break_point<R * threads_per_block, blocks, int>)<<<1024, 512>>>(rowptr, m, break_points, break_points_len);
   FLAT_KERNEL_WRAPPER(R, blocks, threads_per_block);
 }
