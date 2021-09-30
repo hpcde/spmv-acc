bca1b66f38fe4bc3c84f1caa6f2d945784ce72bd
reget <re.masterzero@gmail.com>
2021-09-30 10:49:21 +0800

    perf(kernel-line): set default kernel threads number (`HIP_THREAD`) to `256` for line-one-pass
    change the threads number of kernel `spmv_line_one_pass_kernel` to 256

--- src/acc/hip-line/line_strategy.cpp
+++ src/acc/hip-line/line_strategy.cpp
@@ -9,7 +9,7 @@ void line_sparse_spmv(int trans, const int alpha, const int beta, int m, int n, 
   // const int avg_eles_per_row = ceil(rowptr[m] + 0.0 / m);
   const int avg_eles_per_row = rowptr[m] / m;
   // ROW_NUM * MAX_NNZ_NUM < HIP_THREAD
-  constexpr int HIP_THREAD = 512;
+  constexpr int HIP_THREAD = 256;
   constexpr int R = 2;
   if (avg_eles_per_row <= 5) {
     constexpr int MAX_NNZ_NUM = 5;
