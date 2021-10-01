431239ad52b48f1abb31d78d3d6afa310b7ef538
genshen <genshenchu@gmail.com>
2021-10-02 00:20:35 +0800

    feat(tool): use muellan/clipp lib for cli argument parsing

--- tools/main.cpp
+++ tools/main.cpp
@@ -1,12 +1,11 @@
+#include <cstring>
 #include <iomanip>
 #include <iostream>
 #include <map>
-#include <cstdio>  // printf
-#include <cstdlib> // EXIT_FAILURE
-#include <cstring>
 #include <type_traits>
 #include <vector>
 
+#include "clipp.h"
 #include "global_data.h"
 
 template <typename T> void init_csr_dense_vector(char *buf, std::vector<T> &vec) {
@@ -22,7 +21,7 @@ template <typename T> void init_csr_dense_vector(char *buf, std::vector<T> &vec)
   }
 }
 
-bool read_file(char *path) {
+bool read_file(const char *path) {
   FILE *in = fopen(path, "r");
   if (in == nullptr) {
     return false;
@@ -47,49 +46,84 @@ bool read_file(char *path) {
   return true;
 }
 
-enum SubCmd { nnz = 0, dist = 1 };
+// cli sub commands
+enum class mode { nnz, dist, version, help };
+
+void csr_analyzing(const mode selected, const std::string &csr_path, const std::string &csr_parts);
 
 int main(int argc, char **argv) {
-  const std::string usage = R"""(Usage:
-  csv-reader nnz [file-path]
-  csv-reader dist [file-path]
-  csv-reader --help
-)""";
-  if (argc < 2) {
-    std::cout << usage;
-    return 0;
-  }
-  const std::string sub_cmd_str = std::string(argv[1]);
-  if (sub_cmd_str == "--help") {
-    std::cout << usage;
-    return 0;
-  }
-  SubCmd sub_cmd;
-  if (sub_cmd_str == "nnz") {
-    sub_cmd = nnz;
-  } else if (sub_cmd_str == "dist") {
-    sub_cmd = dist;
-  } else {
-    std::cerr << "unknown sub-command " << sub_cmd_str << std::endl;
-    std::cout << usage;
-    return 1;
-  }
-  if (argc < 3) {
-    std::cerr << "missing file path." << std::endl;
-    return 1;
+  mode selected = mode::help;
+
+  std::string csr_path;
+  std::string csr_parts;
+  auto mode_nnz = (clipp::command("nnz").set(selected, mode::nnz),
+                   clipp::required("-i", "--input").label("--input") & clipp::value("csr file", csr_path),
+                   clipp::option("-p", "--parts") & clipp::value("dividing parts", csr_parts))
+                      .doc("nnz of rows.");
+
+  auto mode_dist = (clipp::command("dist").set(selected, mode::dist),
+                    clipp::required("-i", "--input").label("--input") & clipp::value("csr file", csr_path))
+                       .doc("nnz distribution.");
+
+  std::vector<std::string> wrong_args;
+  auto cli =
+      ((mode_nnz | mode_dist | clipp::command("-h", "--help").set(selected, mode::help).doc("Show this help message.") |
+        clipp::command("-v", "--version").set(selected, mode::version).doc("Display version.")),
+       clipp::any_other(wrong_args));
+
+  if (argc > 1) {
+    // parse command line
+    clipp::parsing_result result = parse(argc, argv, cli);
+    // if parsing error
+    if (!wrong_args.empty()) {
+      for (const auto &arg : wrong_args) {
+        std::cerr << "'" << arg << "' is not a valid argument.\n";
+        return 1;
+      }
+    }
+    if (result.any_error()) {
+      for (const auto &m : result.missing()) {
+        std::cerr << "Error: missing " << m.param()->label() << " after index " << m.after_index() << ".\n";
+        return 1;
+      }
+      // per-argument mapping
+      for (const auto &m : result) {
+        std::cerr << "Error: bad argument at " << m.index() << ": " << m.arg() << " -> " << m.param()->label();
+        std::cerr << '\n';
+        return 1;
+      }
+    }
   }
 
-  char path[1024];
-  strcpy(path, argv[2]);
-  if (!read_file(path)) {
+  switch (selected) {
+  case mode::nnz:
+    csr_analyzing(selected, csr_path, csr_parts);
+    return 0;
+  case mode::dist:
+    csr_analyzing(selected, csr_path, csr_parts);
+    return 0;
+  case mode::help:
+    std::cout << make_man_page(cli, "csr-tool").prepend_section("DESCRIPTION", "csr analyzing tool.");
+    return 0;
+  case mode::version:
+    std::cout << "version 0.1.0" << std::endl
+              << "compiled at " << __TIME__ << ", " << __DATE__ << "." << std::endl
+              << "Copyright (C) 2021 USTB." << std::endl;
+    return 0;
+  }
+  return 0;
+}
+
+void csr_analyzing(const mode sub_cmd, const std::string &csr_path, const std::string &csr_parts) {
+  if (!read_file(csr_path.c_str())) {
     std::cerr << "reading file error." << std::endl;
-    return 1;
+    return;
   }
 
   int m = csr_indptr.size() - 1;
   int n = dense_vector.size();
 
-  if (sub_cmd == nnz) {
+  if (sub_cmd == mode::nnz) {
     bool is_first = true;
     int last_row = 0;
     for (int row_offset : csr_indptr) {
@@ -99,7 +133,9 @@ int main(int argc, char **argv) {
       last_row = row_offset;
       is_first = false;
     }
-  } else {
+  }
+
+  if (sub_cmd == mode::dist) {
     bool is_first = true;
     int last_row = 0;
     std::map<int, int> K;
@@ -121,5 +157,4 @@ int main(int argc, char **argv) {
       std::cout << key << " = " << value << "\n";
     }
   }
-  return 0;
 }
