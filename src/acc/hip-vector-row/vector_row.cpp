29b8197f8a1b973e07ecede56c1f29aa360a8baa
genshen <genshenchu@gmail.com>
2021-11-12 15:40:27 +0800

    feat(kernel-vertor-row): support the case of "wrap size is 32" (Nvidia GPU)

--- src/acc/hip-vector-row/vector_row.cpp
+++ src/acc/hip-vector-row/vector_row.cpp
@@ -3,21 +3,22 @@
 //
 
 #include "vector_row.h"
+#include "building_config.h"
 
 void vec_row_sparse_spmv(int trans, const int alpha, const int beta, int m, int n, const int *rowptr,
                          const int *colindex, const double *value, const double *x, double *y) {
   //  const int avg_eles_per_row = ceil(rowptr[m] + 0.0 / m);
   const int avg_eles_per_row = rowptr[m] / m;
 
-  if (avg_eles_per_row <= 4) {
+  if (avg_eles_per_row <= 4 || __WF_SIZE__ <= 2) {
     NATIVE_VECTOR_KERNEL_WRAPPER(2);
-  } else if (avg_eles_per_row <= 8) {
+  } else if (avg_eles_per_row <= 8 || __WF_SIZE__ <= 4) {
     NATIVE_VECTOR_KERNEL_WRAPPER(4);
-  } else if (avg_eles_per_row <= 16) {
+  } else if (avg_eles_per_row <= 16 || __WF_SIZE__ <= 8) {
     NATIVE_VECTOR_KERNEL_WRAPPER(8);
-  } else if (avg_eles_per_row <= 32) {
+  } else if (avg_eles_per_row <= 32 || __WF_SIZE__ <= 16) {
     NATIVE_VECTOR_KERNEL_WRAPPER(16);
-  } else if (avg_eles_per_row <= 64) {
+  } else if (avg_eles_per_row <= 64 || __WF_SIZE__ <= 32) {
     NATIVE_VECTOR_KERNEL_WRAPPER(32);
   } else {
     NATIVE_VECTOR_KERNEL_WRAPPER(64);
