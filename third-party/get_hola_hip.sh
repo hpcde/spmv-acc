3a5ad3fde8dad1ce286c0cac4d3fcf1f323654c1
reget <re.masterzero@gmail.com>
2021-11-23 10:22:48 +0800

    feat(third-party): add a prompt to set the WARP_SIZE after downloaded hip-hola

--- third-party/get_hola_hip.sh
+++ third-party/get_hola_hip.sh
@@ -6,3 +6,7 @@ git clone https://git.hpcer.dev/PRA/hola-hip.git holahip
 
 cd holahip
 git checkout 20b1669b9c397aca8f629099eb70890c53940da0
+
+echo "completed."
+echo -e "\033[31m Please edit file hip-hola/utils/common.hpp to set the WARP_SIZE to the desired value
+(usually 32 for NVIDIA GPU and 64 for AMD GPU). \033[0m" 
