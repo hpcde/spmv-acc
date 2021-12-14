5e72b0033c75017f8310eb67a774a87a668a8a06
genshen <genshenchu@gmail.com>
2021-12-14 16:53:48 +0800

    fix(third-party): bump hola-hip version to fix building errors from `hipFree` and `CSR<double>` template
    see the latest merge request in hola-hip repo for more details of the building errors.

--- third-party/get_hola_hip.sh
+++ third-party/get_hola_hip.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+set -e
+
 #this file is used for load hola from git.hpcer.dev or github
 
 # or mirror from: https://github.com/hpcde/hola-hip.git
 git clone https://git.hpcer.dev/PRA/hola-hip.git holahip
 
 cd holahip
-git checkout 20b1669b9c397aca8f629099eb70890c53940da0
+git checkout 08a2b06ceb5b5605dabf6842b90ab63b931a5607
 
 echo "completed."
-echo -e "\033[31m Please edit file hip-hola/utils/common.hpp to set the WARP_SIZE to the desired value
+echo "\033[31m Please edit file holahip/hip-hola/utils/common.hpp to set the WARP_SIZE to the desired value
 (usually 32 for NVIDIA GPU and 64 for AMD GPU). \033[0m" 
