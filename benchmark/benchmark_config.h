37432ce6e126d27e67e3eecdabaddd3edf86753f
genshen <genshenchu@gmail.com>
2022-04-12 10:26:20 +0800

    refactor(benchmark): use macro `SPMV_BENCHMARK` to call spmv benchmark

---
+++ benchmark/benchmark_config.h
@@ -0,0 +1,30 @@
+//
+// Created by genshen on 2022/4/12.
+//
+
+#ifndef SPMV_ACC_BENCHMARK_CONFIG_H
+#define SPMV_ACC_BENCHMARK_CONFIG_H
+
+// spmv-acc
+constexpr bool ENABLE_SPMV_ACC_DEFAULT = false;
+constexpr bool ENABLE_SPMV_ACC_ADAPTIVE = true;
+constexpr bool ENABLE_SPMV_ACC_BLOCK_ROW = true;
+constexpr bool ENABLE_SPMV_ACC_FLAT = true;
+constexpr bool ENABLE_SPMV_ACC_LIGHT = true;
+constexpr bool ENABLE_SPMV_ACC_LINE = true;
+constexpr bool ENABLE_SPMV_ACC_THREAD_ROW = true;
+constexpr bool ENABLE_SPMV_ACC_VECTOR_ROW = true;
+constexpr bool ENABLE_SPMV_ACC_WF_ROW = true;
+constexpr bool ENABLE_SPMV_ACC_LE_ROW = true;
+
+// rocm
+constexpr bool ENABLE_ROC_VECTOR_ROW = true;
+constexpr bool ENABLE_ROC_ADAPTIVE = true;
+constexpr bool ENABLE_HIP_HOLA = true;
+
+// cuda
+constexpr bool ENABLE_CU_SPARSE = true;
+constexpr bool ENABLE_CUB = true;
+constexpr bool ENABLE_HOLA = true;
+
+#endif // SPMV_ACC_BENCHMARK_CONFIG_H
