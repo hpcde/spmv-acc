2a4dbe89fc14ee510521fa2b9a60fc2ea260a12a
yuanjie <re.masterzero@gmail.com>
2023-04-02 15:00:54 +0800

    fix(acsr): fix acsr compute error

--- benchmark/acsr/ACSR.h
+++ benchmark/acsr/ACSR.h
@@ -4,12 +4,7 @@
 #include "../utils/benchmark_time.h"
 #include "api/types.h"
 
-
-template<class T>
-void acsr_driver(T *values, int *col_idx, int *row_off, T *x, T *y, int m, int n, int nnz,BenchmarkTime *bmt);
-void acsr(int trans, const csr_desc<int, double> h_csr_desc, const csr_desc<int, double> d_csr_desc, const double *x,
+void acsr(int trans, const int alpha, const csr_desc<int, double> h_csr_desc, const csr_desc<int, double> d_csr_desc, const double *x,
           double *y, BenchmarkTime *bmt);
-
-
 
 #endif
