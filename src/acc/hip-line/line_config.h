25905b3c2cf77c46abaf9024a1345bc4b5af62bd
yuanjie <re.masterzero@gmail.com>
2023-04-02 19:39:26 +0800

    update config adaptive line enhance method

--- src/acc/hip-line/line_config.h
+++ src/acc/hip-line/line_config.h
@@ -5,6 +5,6 @@
 #ifndef SPMV_ACC_LINE_CONFIG_H
 #define SPMV_ACC_LINE_CONFIG_H
 
-#define LINE_GLOBAL_LOAD_X2 // if defined, we load 2 double or 2 int in each loop.
+// #define LINE_GLOBAL_LOAD_X2 // if defined, we load 2 double or 2 int in each loop.
 
 #endif
