7a0d60ea8daec8d21f94ba6e41390e64fc596259
yuanjie <re.masterzero@gmail.com>
2023-04-06 21:10:46 +0800

    feat(benchmark-merge-path): reduction support process mutiple items per thread

--- benchmark/merge-path/merge_path_reduction.h
+++ benchmark/merge-path/merge_path_reduction.h
@@ -10,50 +10,59 @@ __global__ void __launch_bounds__(BLOCK_THREAD_NUM)
     reduction(const int alpha, int nnz, int *__restrict__ S, KeyValuePair<int, T> *__restrict__ r,
               const I *__restrict__ rowptr, const I *__restrict__ colindex, const T *__restrict__ value,
               const T *__restrict__ x, T *__restrict__ y) {
+  static_assert(ITEMS_PER_THREAD <= 32, "ITEMS_PER_THREAD must less equal than 32.");
   const int global_block_id = blockIdx.x;
   const int block_thread_id = threadIdx.x;
   const int block_row_begin = S[global_block_id];
   const int block_row_end = S[global_block_id + 1];
+  const int block_idx_begin = global_block_id * BLOCK_THREAD_NUM * ITEMS_PER_THREAD;
 
   using BlockScanT = cub::BlockScan<KeyValuePair<int, T>, BLOCK_THREAD_NUM>;
   __shared__ typename BlockScanT::TempStorage temp_storage_for_scan;
   using ReduceOpT = ReduceByKeyOp;
-  KeyValuePair<int, T> pair(block_row_end, static_cast<T>(0));
   ReduceOpT op;
-
-  const int idx = global_block_id * BLOCK_THREAD_NUM * ITEMS_PER_THREAD + block_thread_id;
-  bool is_last;
+  KeyValuePair<int, T> pair[ITEMS_PER_THREAD];
+  unsigned int is_last = 0;
+#pragma unroll ITEMS_PER_THREAD
+  for (int i = 0; i < ITEMS_PER_THREAD; ++i) {
+    pair[i] = {block_row_end, static_cast<T>(0)};
+    const int idx = block_idx_begin + ITEMS_PER_THREAD * block_thread_id + i;
 #ifdef REDUCE_LINEAR_SEARCH
-  // linear search
-  int next_row_idx;
-  for (int row = block_row_begin + 1; row <= block_row_end; ++row) {
-    next_row_idx = rowptr[row];
-    if (idx < next_row_idx) {
-      pair.key = row - 1;
-      break;
+    // linear search
+    int next_row_idx;
+    for (int row = block_row_begin + 1; row <= block_row_end; ++row) {
+      next_row_idx = rowptr[row];
+      if (idx < next_row_idx) {
+        pair[i].key = row - 1;
+        break;
+      }
     }
-  }
-  is_last = (idx + 1 == next_row_idx);
+    is_last |= (idx + 1 == next_row_idx) << i;
 #endif
 #ifdef REDUCE_BINARY_SEARCH
-  // binary search
-  pair.key = largest_less_equal_binary_search(block_row_begin, block_row_end + 1, rowptr, idx);
-  is_last = (idx + 1 == rowptr[pair.key + 1]);
+    // binary search
+    pair[i].key = largest_less_equal_binary_search(block_row_begin, block_row_end + 1, rowptr, idx);
+    is_last |= (idx + 1 == rowptr[pair[i].key + 1]) << i;
 #endif
-  if (idx < nnz) {
-    pair.val = alpha * value[idx] * x[colindex[idx]];
-  }
-  __syncthreads();
-  BlockScanT(temp_storage_for_scan).InclusiveScan(pair, pair, op);
-  __syncthreads();
-  if (is_last) {
-    y[pair.key] = pair.val;
-  }
-  if (block_thread_id == BLOCK_THREAD_NUM - 1) {
-    if (is_last) {
-      pair.val = 0;
+    if (idx < nnz) {
+      pair[i].val = alpha * value[idx] * x[colindex[idx]];
     }
-    r[global_block_id] = pair;
+  }
+  __syncthreads();
+  BlockScanT(temp_storage_for_scan).InclusiveScan<ITEMS_PER_THREAD>(pair, pair, op);
+  __syncthreads();
+#pragma unroll ITEMS_PER_THREAD
+  for (int i = 0; i < ITEMS_PER_THREAD; ++i) {
+    if (is_last & 0b1) {
+      y[pair[i].key] = pair[i].val;
+    }
+    if (block_thread_id == BLOCK_THREAD_NUM - 1 && i == ITEMS_PER_THREAD - 1) {
+      if (is_last & 0b1) {
+        pair[i].val = 0;
+      }
+      r[global_block_id] = pair[i];
+    }
+    is_last >>= 1;
   }
 }
 
