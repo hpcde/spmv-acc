1d2ff41ddb9388e5414be51e5d80b9187f90486d
genshen <genshenchu@gmail.com>
2024-12-30 15:27:33 +0800

    build(pkg): use pkg tool to download dependencies: cub, holaspmv, hola-hip
    pkg tool can be download via https://github.com/genshen/pkg

--- benchmark/hola-hip/hola-hip_source_list.cmake
+++ benchmark/hola-hip/hola-hip_source_list.cmake
@@ -1,12 +1,12 @@
 set(SP_LIB_HEADER ${SP_LIB_HEADER}
         ${SP_LIB_SRC_BASE}/hola-hip/spmv.h
-        ${CMAKE_SOURCE_DIR}/third-party/holahip/hip-hola/d_csr.h
-        ${CMAKE_SOURCE_DIR}/third-party/holahip/hip-hola/hola_vector.h
-        ${CMAKE_SOURCE_DIR}/third-party/holahip/hip-hola/hola_spmv.h
+        ${HOLA_HIP_SRC_DIR}/hip-hola/d_csr.h
+        ${HOLA_HIP_SRC_DIR}/hip-hola/hola_vector.h
+        ${HOLA_HIP_SRC_DIR}/hip-hola/hola_spmv.h
         )
 
 set(SP_LIB_SOURCE ${SP_LIB_SOURCE}
         ${SP_LIB_SRC_BASE}/hola-hip/spmv.cpp
-        ${CMAKE_SOURCE_DIR}/third-party/holahip/hip-hola/hola_spmv.cpp
-        ${CMAKE_SOURCE_DIR}/third-party/holahip/hip-hola/d_csr.cpp
+        ${HOLA_HIP_SRC_DIR}/hip-hola/hola_spmv.cpp
+        ${HOLA_HIP_SRC_DIR}/hip-hola/d_csr.cpp
         )
