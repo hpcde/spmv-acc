ff6704464c43a1e213f06455b65944b85ee030d0
genshen <genshenchu@gmail.com>
2025-05-17 23:15:56 +0800

    build(cmake): add cmake presets for ROCm and CUDA platform building
    Now, min cmake version is 3.21

--- scripts/hipcc-nv-wrapper.sh
+++ scripts/hipcc-nv-wrapper.sh
@@ -1,27 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
-# this wrapper script is used for nvlink/hipcc to remove `-std=gnu++14` argument.
-
-LINKER=/opt/compilers/rocm/4.2.0/bin/hipcc
+LINKER="hipcc"
 newcmd="$LINKER"
-REMOVE="-std=gnu++14"
+flag_xcpp_appear=0
 
 for arg in $@
 do
   case $arg in
-    $REMOVE) # remove argument
+    "-hc") # 移除参数 `-hc`
       ;;
 
     # OpenMP
-    "-fopenmp")
+    "-fopenmp") # 添加参数 `-Xcompiler`
      newcmd="$newcmd -Xcompiler -fopenmp";;
 
-    "-fopenmp=libomp")
-      newcmd="$newcmd -Xcompiler -fopenmp";;
+    "-fpch-preprocess")
+     newcmd="$newcmd -Xcompiler -fpch-preprocess";;
+
+    "-xc++")
+     flag_xcpp_appear=1
+     newcmd="$newcmd -Xcompiler -xc++";;
+
+    "-fdiagnostics-color=always")
+    newcmd="$newcmd -Xcompiler -fdiagnostics-color=always";;
+
     *)
       newcmd="$newcmd $arg";;
   esac
 done
 
+
+#if [ "$flag_xcpp_appear" -eq 1 ]; then
+  echo "flag '-xc++' appears in this commane." >&2
+  echo $newcmd >&2
+#fi
 # Finally execute the new command
 exec $newcmd
